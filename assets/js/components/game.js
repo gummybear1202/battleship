ca7307cc4ab46fc19b6d93a7009f3b255337257b
ros <rosalinesu@gmail.com>
2017-12-01 16:49:30 -0500

    first

---
+++ assets/js/components/game.js
@@ -0,0 +1,10 @@
+// attributes to Nat Tuck's lecture notes and the Hangman repo
+import React from "react";
+
+export default class Game extends React.Component {
+  render() {
+    return (
+      <h1> this is game. </h1>
+    );
+  }
+};
